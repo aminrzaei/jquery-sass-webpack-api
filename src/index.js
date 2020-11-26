5edf30d325e907b014f0cc92d0ef64be4a03034b
Amin <my.aminrezaei@gmail.com>
2020-11-27 01:39:36 +0330

    header added

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 import $ from 'jquery';
 import sass from './scss/style.scss';
 
-$('.main').css('background-color', 'yellow');
+// $('.main').css('background-color', 'yellow');
+
+// navbar__item     &:hover::after {
+//     width: 120%;
+//     left: -8%;
+//   }
+
+$('.navbar__item').hover(
+  function () {
+    $(this).addClass('navbar__item--show');
+  },
+  function () {
+    $(this).removeClass('navbar__item--show');
+  }
+);
