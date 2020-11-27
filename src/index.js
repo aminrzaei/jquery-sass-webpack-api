239bb4297167d78733ffc7b273d05bd9cab2ee13
Amin <my.aminrezaei@gmail.com>
2020-11-27 17:24:31 +0330

    compelete header

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import $ from 'jquery';
 import sass from './scss/style.scss';
 
-// $('.main').css('background-color', 'yellow');
-
-// navbar__item     &:hover::after {
-//     width: 120%;
-//     left: -8%;
-//   }
-
-$('.navbar__item').hover(
-  function () {
-    $(this).addClass('navbar__item--show');
+$('.navbar__heading').hover(
+  function (e) {
+    const heading = e.target.closest('.navbar__heading');
+    $('.navbar__underline').css({
+      width: `${heading.clientWidth}`,
+      left: `${heading.offsetLeft}px`,
+      transform: 'scaleX(1)',
+    });
   },
   function () {
-    $(this).removeClass('navbar__item--show');
+    $('.navbar__underline').css({
+      transform: 'scaleX(0)',
+    });
   }
 );
